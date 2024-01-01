7affa0f7917a8a581cb691f0918fc9aef22a9bff
hideki-tomiyama <149802016+hideki-tomiyama@users.noreply.github.com>
2024-01-01 16:16:33 +0900

    課題_007提出(初回)

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+ <head>
+   <title>PHP課題_007</title>
+   <meta charset="UTF-8">
+ </head>
+
+ <body>
+   <p>
+     <?php
+       $merchandise = ["name" => "onion", "price" => 200, "weigth" => 160];
+       print_r($merchandise);
+      ?>
+    </p>
+ </body>
+</html>
